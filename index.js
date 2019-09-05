503dc5624881eac5a7a04bb1164bbe5ec91b7dcd
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-09-05 14:28:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,21 +9,68 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+ //initialize object
+ var newCart = {
+   itemName: "itemName",
+   itemPrice: "itemPrice"
+ }; 
+  var price = Math.floor(Math.random()*Math.floor(100));
+  newCart.itemName = [item];
+  newCart.itemPrice = price;
+  cart.push(newCart);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+  var length = cart.length;
+  var list = [];
+  var i=0;
+  if (length === 0) {
+    return `Your shopping cart is empty.`
+  } else if (length === 1){
+    list.push(` ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+   return `In your cart, you have${list.toString()}`;
+    } while (i < length-1){
+      list.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+      i++;
+      } if (i === length-1){
+        list.push(` and ${cart[i].itemName} at $${cart[i].itemPrice}`);
+        } return `In your cart, you have${list.toString()}.`;
+    }
 
 function total() {
-  // write your code here
+  var length = cart.length;
+  var total = 0;
+  for (var i=0; i < length; i++){
+    var price = parseInt(cart[i].itemPrice, 10);
+    total = total + price;
+  } return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var length = cart.length;
+  var items = [];
+  
+    for (var i=0; i < length; i++){
+    //items[i] = cart[i].itemName;
+    items.push(cart[i].itemName);
+    } 
+    
+   
+    for (var n=0; n < length; n++){
+    if (items[n] !== item){ 
+    } else if (items[n] === item){
+      cart.splice(n, 1);
+      //var newCart = cart;s
+     // return newCart;
+    } else return "That item is not in your cart."
+  } 
 }
+ 
+  
+
 
 function placeOrder(cardNumber) {
   // write your code here
