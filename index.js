97020f08feda9db46dc867a93462d96b77f1256c
Fabiana Fornerino <fabianafornerino@gmail.com>
2019-09-06 13:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   }
   let totalCost = total()
   for (let i=0; i <cart.length; i++){
-    delete cart[i]
+    cart[i].pop()
   }
   
   return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
